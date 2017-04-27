1ec7b680656722533eec4558020d5601b7a8a867
pandaro <prandipadaro@gmail.com>
2017-04-27 11:55:58 +0200

    starting re-draw trading

--- mods/money/init.lua
+++ mods/money/init.lua
@@ -89,3 +89,113 @@ minetest.register_node("money:shop", {
 	end
 })
 
+minetest.register_node("money:trader", {
+	description = "trader",
+	tiles = {"money_shop_top.png", "money_shop_bottom.png", "money_shop.png","money_shop.png"},
+	groups = {choppy = 3},
+	paramtype2 = "facedir",
+	
+	on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+		local inv = meta:get_inventory()
+		inv:set_size("main", 8*4)
+		inv:set_size("side", 8*4)
+		local invs = meta:from_table({
+			inventory = {
+			main = {
+				[1] = "default:dirt", [2] = "", [3] = "", [4] = "",
+				[5] = "", [6] = "", [7] = "", [8] = "", [9] = "",
+				[10] = "", [11] = "", [12] = "", [13] = "",
+				[14] = "default:cobble", [15] = "", [16] = "", [17] = "",
+				[18] = "", [19] = "", [20] = "default:cobble", [21] = "",
+				[22] = "", [23] = "", [24] = "", [25] = "", [26] = "",
+				[27] = "", [28] = "", [29] = "", [30] = "", [31] = "",
+				[32] = ""},
+			
+			side = {
+				[1] = "default:stone", [2] = "", [3] = "", [4] = "",
+				[5] = "", [6] = "", [7] = "", [8] = "", [9] = "",
+				[10] = "", [11] = "", [12] = "", [13] = "",
+				[14] = "torch:torch", [15] = "", [16] = "", [17] = "",
+				[18] = "", [19] = "", [20] = "default:dirt", [21] = "",
+				[22] = "", [23] = "", [24] = "", [25] = "", [26] = "",
+				[27] = "", [28] = "", [29] = "", [30] = "", [31] = "",
+				[32] = ""}
+			},
+			
+		})
+		local background = 'background[0;0;0,0;money_shop_top.png;true]'--useless
+		local size='size[12,12,false]'
+		local container0='container[5,5]'
+		local listring = 'listring[context;main]'
+		local listring2 = 'listring[context;side]'
+		local list = 'list[context;main;0,0;8,4;]'
+		local textlist = 'textlist[0,1;1,1;<name>;albero,foglia,tronco,radice,seme,frutto,pangom]'
+		local box = 'box[0,0;1,1;#FFFFFF]'
+		local bgcolor = 'bgcolor[#ffffff;true]'
+		local tabheader = 'tabheader[0,0;header;sasso,carta,forbice;forbice;false;true]'
+		local dropdown = 'dropdown[0,0;1;casa;tetto,muri,porte;2]'
+		local checkbox ='checkbox[0,0;vero;vero;true]'
+		local scrollbar = 'scrollbar[0,0;1,1;horizontal;scroll;200]'
+		--local itembutton = 'item_image_button[0,0;1,1;' .. table.insert(items,v:to_table().name) .. ';torch;torch]'
+		print(tostring(inv))
+		local items = {}
+		local infos = ''
+		local prices = ''
+		local buys = ''
+		local sells = ''
+		local index = 1
+		for i,v in pairs(meta:to_table().inventory.side) do
+			print(tostring(i))
+			print(tostring(v:to_table()))
+			
+			if v:to_table() and v:to_table().name then
+				local name = v:to_table().name
+				local fnameinfo = name..'info'
+				local fnamebuy = name .. 'buy'
+				local fnamesell = name..'sell'
+				local _index = tostring(index)
+				local labelpos = tostring(index + 1)
+				local price = v:to_table().price or math.random(1,1000)
+				price = tostring(price)
+				table.insert(items,'item_image_button[1,'.. _index ..';2,2;' .. v:to_table().name .. ';' .. v:to_table().name .. ';' .. v:to_table().name .. ']')
+				infos = infos ..'button[3,'.. _index ..';1,1;'.. fnameinfo ..';info]'
+				prices = prices ..'label[3,'.. labelpos ..';$'.. price ..']'
+				buys = buys .. 'button[5,'.. _index ..';2,2;'.. fnamebuy ..';buy one]'
+				buys = buys .. 'button[5,'.. labelpos ..';2,2;'.. fnamesell ..';sell one]'
+
+				index = index+2
+			end
+			
+		end
+		itemslist = '' 
+		for i,v in pairs(items) do
+			itemslist = itemslist .. v
+		end
+			
+			
+		
+		meta:set_string('formspec', size ..itemslist..infos..prices ..buys)
+	end,
+	on_receive_fields = function(pos, formname, fields, sender)
+		money.fields(formname,fields)
+	end,
+})
+
+function money.fields(formname,fields)
+	for name,value in pairs(fields) do
+		print(tostring(formname))
+		print(tostring(name))
+		print(tostring(value))
+		local aaa=money.received[value]()
+		
+	end
+end
+money.received = {}
+money.received.info = function(value)
+	print(tostring('informations'))
+end
+money.received.buy = function(value)
+	print(tostring('direct buy'))
+end
+
